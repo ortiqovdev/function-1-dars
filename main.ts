ba00a130dda9a61adec33f8cebb74b9f29183a53
ortiqovdev <akmalbekortiqov7976@gmail.com>
2023-01-28 12:06:22 +0500

    funkisya 15 dars 1 mavzu

---
+++ main.ts
@@ -0,0 +1,10 @@
+// type Save = { name: string, lastname: string }
+// function join(o: Save) {
+//     console.log(names.name, names.lastname);
+
+// }
+// let names: Save = {
+//     name: 'Diyorbek',
+//     lastname: 'Ravshanbekov'
+// }
+//join(names)
